e8806b4995465f931272b0c5c2b2fb221d79b00f
Cold Code <dee_femifose@yahoo.com>
2022-02-03 17:00:30 -0600

    Initial Commit

---
+++ Slots Demo/Slots_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_DemoApp.swift
+//  Slots Demo
+//
+//  Created by Phemmy DOF on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Slots_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
